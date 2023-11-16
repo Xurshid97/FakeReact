8d440cc896236dede3439b9f1df7b85bf5a2ded1
Xurshid97 <fantasticidea3000@gmail.com>
2023-11-17 00:14:32 +0100

    css added

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -3,13 +3,19 @@ import { Outlet, Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div className='cards'>
-      <Link to={'category/jewelery'}>Jewelery</Link>
-      <Link to={'category/electronics'}>Electronics</Link>
-      <Link to={"category/men's clothing"}>Men</Link>
-      <Link to={"category/women's clothing"}>Women</Link>
-      <Outlet />
-    </div>
+      <>
+        
+      <div className='links'>
+        <Link to={'category/jewelery'}>Jewelery</Link>
+        <Link to={'category/electronics'}>Electronics</Link>
+        <Link to={"category/men's clothing"}>Men</Link>
+        <Link to={"category/women's clothing"}>Women</Link>
+      </div>
+      
+      <div className='cards'>
+      <Outlet></Outlet>
+      </div>
+      </>
   )
 }
 
