c874f8169cd5250b9474cdea80ef445a33818c6d
Xurshid97 <fantasticidea3000@gmail.com>
2023-11-18 17:46:10 +0100

    search params is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import {Routes, Route} from 'react-router-dom'
 import './App.css'
 import Home from './assets/Home'
-import Category from './assets/Category'
 import Item from './assets/Item'
 
 function App() {
@@ -9,8 +8,6 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Home />}>
-        <Route path='category/:name' element={<Category />}></Route>
-        <Route index element={<Category />}></Route>
       </Route>
       <Route path='item/:id' element={<Item />}></Route>
     </Routes>
--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -1,21 +1,63 @@
-import React, {useState} from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Outlet, useSearchParams, useNavigate } from 'react-router-dom'
 
 function Home() {
+  let navigate = useNavigate()
+  let [items, setItems] = useState([])
+  let [searchPar, setSearchPar] = useSearchParams()
+
+  let btnClicked = (e) => {
+    setSearchPar({ catgeory: `${e.target.className}` })
+  }
+  let viewItem = (e)=> {
+      navigate(`/item/${e.target.getAttribute('itemid')}`)
+  }
+  let api = null
+  if (searchPar.get('catgeory')) {
+    if (searchPar.get('catgeory') == 'categories') {
+      api = `https://fakestoreapi.com/products`
+    }
+    else {
+      api = `https://fakestoreapi.com/products/category/${searchPar.get('catgeory')}`
+    }
+  } else {
+    api = 'https://fakestoreapi.com/products'
+  }
+
+  useEffect(() => {
+    fetch(api).
+      then(res => res.json()).
+      then((data) => {
+        setItems(data)
+      })
+  }, [searchPar, api])
+
   return (
-      <>
-        
+    <>
+
       <div className='links'>
-        <Link to={'category/jewelery'}>Jewelery</Link>
-        <Link to={'category/electronics'}>Electronics</Link>
-        <Link to={"category/men's clothing"}>Men</Link>
-        <Link to={"category/women's clothing"}>Women</Link>
+        <button onClick={btnClicked} className='categories'>All</button>
+        <button onClick={btnClicked} className='jewelery'>Jewelery</button>
+        <button onClick={btnClicked} className='electronics'>Electronics</button>
+        <button onClick={btnClicked} className="men's clothing">Men</button>
+        <button onClick={btnClicked} className="women's clothing">Women</button>
       </div>
-      
+
       <div className='cards'>
-      <Outlet></Outlet>
+        <Outlet />
+        {
+          items && items.map((item) => {
+            return (
+              <div key={item.id} className='itemCard'>
+                <img src={item.image} alt="" />
+                {item.title}
+                <button onClick={viewItem} itemID={item.id}>View</button> 
+              </div>
+            )
+          })
+        }
       </div>
-      </>
+    </>
   )
 }
 
